96bca1156df683f27868515f706ff378c5bf2a66
FrancisBacon61 <klyaverbl@gmail.com>
2026-07-06 17:12:40 +0300

    Delete src directory

--- src/main/java/com/example/pocketmoney/domain/usecase/GetAccountUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.pocketmoney.domain.usecase
-
-import com.example.pocketmoney.domain.repository.FinanceRepository
-import com.example.pocketmoney.domain.models.Account
-import kotlinx.coroutines.flow.Flow
-
-class GetAccountUseCase(private val repository: FinanceRepository) {
-    operator fun invoke(): Flow<Account?> = repository.getAccount()
-}
--- src/main/java/com/example/pocketmoney/ui/navigation/Screen.kt
+++
@@ -1,7 +0,0 @@
-package com.example.pocketmoney.ui.navigation
-
-sealed class Screen(val route: String) {
-    object Home : Screen("home")
-    object Transactions : Screen("transactions")
-    object Settings : Screen("settings")
-}
